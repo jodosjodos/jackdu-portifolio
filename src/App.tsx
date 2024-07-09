249f606f126e7a9516717720608a7325bf6b74ea
SylvinIsamaza <isamazasylvin@gmail.com>
2024-07-10 01:05:01 +0200

    [FIX] fixing build error

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
+
 
 import './App.css'
 import {  HashRouter, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
-import Header from './components/Header'
+
 import AboutUsPage from './pages/AboutUsPage'
 import TimelineIcons from './components/Timeline'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <HashRouter>
--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import Header from "../components/Header";
 import SectionTitle from "../components/SectionTitle";
 import { analytics, skills } from "../statics/data";
