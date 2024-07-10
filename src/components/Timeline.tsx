c55a7cff70a3dd2b30d85aa0acb515093fa53f94
SylvinIsamaza <isamazasylvin@gmail.com>
2024-07-10 10:05:53 +0200

    [FEAT] resume page addition

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,43 +1,26 @@
-export default function TimelineIcons(): JSX.Element {
-  return (
-    <div className="w-full px-[20px]">
-      <h3 className="mb-6 ml-3 text-2xl font-bold">Latest News</h3>
+import { ReactNode } from "react";
 
-      <ol className="border-l-2 border-info-100">
-        {/* <!--First item--> */}
+export default function TimeLine({ items, title }: { items: ReactNode[], title: string[] }): JSX.Element {
+  console.log(items)
+  return (
+    <div className="w-full py-[20px] px-[10px]">
+      {items.map((child, index) => (
+        <ol className="border-l-2 border-info-100">
+
         <li>
           <div className="flex-start md:flex">
             <div className="-ml-[13px] flex h-[25px] w-[25px] items-center justify-center rounded-full bg-info-100 text-info-700">
-              <div className="bg-primary w-[16px] mt-[-20px] ml-[-px] h-[16px] rounded-full"></div>
+              <div className="bg-[#64d36e] w-[16px] mt-[-20px] ml-[3px] h-[16px] rounded-full"></div>
             </div>
-            <div className="mb-10 ml-6 block max-w-md rounded-lg bg-neutral-50 p-6 shadow-md shadow-black/5 dark:bg-neutral-700 dark:shadow-black/10">
-              <div className="mb-4 flex justify-between">
-                <a
-                  href="#!"
-                  className="text-sm text-info transition duration-150 ease-in-out hover:text-info-600 focus:text-info-600 active:text-info-700"
-                >
-                  New Web Design
-                </a>
-                <a
-                  href="#!"
-                  className="text-sm text-info transition duration-150 ease-in-out hover:text-info-600 focus:text-info-600 active:text-info-700"
-                >
-                  04 / 02 / 2022
-                </a>
-              </div>
-              <p className="mb-6 text-neutral-700 dark:text-neutral-200">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-                scelerisque diam non nisi semper, et elementum lorem ornare.
-                Maecenas placerat facilisis mollis. Duis sagittis ligula in
-                sodales vehicula.
-              </p>
-             
+            <div className="flex mt-[-2px]  px-[20px] flex-col gap-[5px]">
+              <h2 className="text-[#64d36e] font-semibold text-[18px]">{title[index]}</h2>
+              {child}
             </div>
           </div>
         </li>
-        
-   
       </ol>
+      ))}
+      
     </div>
   );
 }
--- src/statics/data.ts
+++ src/statics/data.ts
@@ -86,3 +86,47 @@ export const services = [
     description: "Defi and Dapps platform | Smart contracts | NFT |",
   },
 ];
+
+
+export const experience:{title:string,date:string,company:string,location:string,responsibilities:string[]}[] = [
+  {
+    title: "LEAD FULL STACK DEVELOPER",
+    date: "Jan 2022 - Nov 2023",
+    company: "Hamlet",
+    location: " Columbia, Maryland, United States",
+    responsibilities: [
+      "Led a team of Junior and Mid Developers, overseeing project designations and creating workflows for efficient development",
+      "Maintained, developed, upgraded and enhanced the company's websites and internal systems, ensuring a high level of performance and usability",
+      "Led the planning, tracking, and documentation of team progress, ensuring timely completion of projects.",
+      "Created and implemented standardized codes for all company websites and systems, providing guidance for other developers and improving overall consistency.",
+      "Conceptualized and implemented database structures, architectures, and relationships, optimizing data management and performance.",
+      "Ensured that all modifications to the company's websites and systems were fully tested prior to deployment, minimizing the risk of errors and bugs",
+      "Collaborated with other staff to come up with improvements, enhancements, and bug fixes to the company's websites and ordering systems, driving innovation and customer satisfaction.",
+      "Demonstrated expertise in managing and deploying scalable cloud infrastructure using AWS services, including EC2, S3, and RDS, resulting in increased system reliability and cost optimization",
+      "Proficient in a wide range of technologies including Reactjs, Typescript, Vuejs, Angular, Node Js, React Native, Nestjs, Django, Mysql, MongoDB,Postgresql,AWS."
+    ]
+  }
+  ,{
+    title: "SMART CONTRACT,WEB3 DEVELOPER",
+    date: "Nov 2016 - Nov 2021",
+    company: " Hamlet",
+    location: "United States",
+    responsibilities: [
+      "Specialized in developing, integrating and deploying smart contracts on Ethereum/Solana Network using Hardhat/Truffle/Remix.",
+      "Expertise in building NFT Marketplace, NFT games, NFT Foundry Website, NFT Art Image by rarity, OpenSea, Pinata, Metadata Generate, Staking, Reveal Function",
+      "Proficient in ERC-20, ERC-721, ERC-1155, Solidity, Rust,Cardano."
+     
+    ]
+  }
+  ,{
+    title: "FULL-STACK DEVELOPER",
+    date: "Nov 2012 - Oct 2016",
+    company: "Webhelp",
+    location: "France",
+    responsibilities: [
+      
+     
+    ]
+  },
+ 
+]
